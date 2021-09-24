80d74c3aef9a7b40e1698ed7c6542e7df5ec01d4
unknown <dennis10.havinanda@gmail.com>
2021-09-24 09:32:11 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama	: Dennis Havinanda");
+		System.out.println("NIM	: 6706210119");
+		System.out.println("Kelas	: D3RPLA_45_05");
+	}	
+}
